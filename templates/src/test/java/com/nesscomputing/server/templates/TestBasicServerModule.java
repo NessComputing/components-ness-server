8ef72f6da974dad2070a7bc3c0409d9156931ecd
Henning Schmiedehausen <henning@likeness.com>
2012-08-09 11:39:53 -0700

    add templates module to replace the BasicJerseyServer module from ness-jersey-jersey

---
+++ templates/src/test/java/com/nesscomputing/server/templates/TestBasicServerModule.java
@@ -0,0 +1,24 @@
+package com.nesscomputing.server.templates;
+
+import org.junit.Test;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.Stage;
+import com.nesscomputing.config.Config;
+import com.nesscomputing.config.ConfigModule;
+import com.nesscomputing.lifecycle.guice.LifecycleModule;
+
+public class TestBasicServerModule
+{
+    @Test
+    public void testSimple()
+    {
+        final Config config = Config.getEmptyConfig();
+
+        Guice.createInjector(Stage.PRODUCTION,
+                             new LifecycleModule(),
+                             new ConfigModule(config),
+                             new BasicServerModule(config));
+    }
+}
